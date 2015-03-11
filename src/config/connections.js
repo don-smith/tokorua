d83920da99ee4557d38a617376715091bce8d1e0
Don Smith <locksmithdon@gmail.com>
2015-03-11 18:20:42 +1300

    Fixed db connection info for production

--- src/config/connections.js
+++ src/config/connections.js
@@ -56,13 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+
+  mongodb: {
+      adapter: 'sails-mongo',
+      host: process.env.MONGO_HOST,
+      port: process.env.MONGO_PORT,
+      user: process.env.MONGO_USER,
+      password: process.env.MONGO_PASS,
+      database: process.env.MONGO_DB
   },
 
   /***************************************************************************
